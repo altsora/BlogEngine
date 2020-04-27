438201f42f9e67d98f7bf3d3afe7b7c2e40aa388
altsora <vergun_a.s@mail.ru>
2020-04-27 20:12:07 +0300

    Вся работа с локальным хранилищем

---
+++ src/main/java/main/BookController.java
@@ -0,0 +1,31 @@
+package main;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import response.Book;
+
+import java.util.List;
+
+@RestController
+public class BookController {
+
+    @GetMapping("/books/")
+    public List<Book> list() {
+        return Storage.getAllBooks();
+    }
+
+    @PostMapping("/books/")
+    public int add(Book book) {
+        return Storage.addBook(book);
+    }
+
+    @GetMapping("/books/{id}")
+    public ResponseEntity get(@PathVariable int id) {
+        Book book = Storage.getBook(id);
+        if (book == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return new ResponseEntity(book, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/main/Storage.java
@@ -0,0 +1,32 @@
+package main;
+
+import response.Book;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Storage {
+    private static int currentId = 1;
+    private static HashMap<Integer ,Book> books = new HashMap<>();
+
+    public static List<Book> getAllBooks() {
+        ArrayList<Book> bookList = new ArrayList<>();
+        bookList.addAll(books.values());
+        return bookList;
+    }
+
+    public static int addBook(Book book) {
+        int id = currentId++;
+        book.setId(id);
+        books.put(id, book);
+        return id;
+    }
+
+    public static Book getBook(int bookId) {
+        if (books.containsKey(bookId)) {
+            return books.get(bookId);
+        }
+        return null;
+    }
+}
