3723f921813c03719e0ca99221d1d5336a087f5d
altsora <vergun_a.s@mail.ru>
2020-05-01 00:37:06 +0300

    Добавлены классы User, Post и перечисление ModerationStatusType. В тестовой базе работает

---
+++ src/main/java/main/model/Post.java
@@ -0,0 +1,46 @@
+package main.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "posts")
+@NoArgsConstructor
+@Data
+@ToString
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "is_active", nullable = false)
+    private boolean isActive;
+
+    //@Column(name = "moderation_status", columnDefinition = "ENUM('NEW', 'ACCEPTED', 'DECLINED')")
+    @Column(name = "moderation_status")
+    @Enumerated(EnumType.STRING)
+    private ModerationStatusType moderationStatus;
+
+    @Column(name = "moderator_id")
+    private int moderatorId;
+
+    @Column(name = "user_id", nullable = false)
+    private int userId;
+
+    @Column(name = "time", nullable = false)
+    private Date time;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "text", nullable = false)
+    private String text;
+
+    @Column(name = "view_count", nullable = false)
+    private int viewCount;
+}
---
+++ src/main/java/main/model/User.java
@@ -0,0 +1,39 @@
+package main.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "is_moderator", nullable = false)
+    private boolean isModerator;
+
+    @Column(name = "reg_time", nullable = false)
+    private Date regTime;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "photo")
+    private String photo;
+}
