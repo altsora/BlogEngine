50ed3a5042edd52fe808aded89a4d71356b41d6f
altsora <vergun_a.s@mail.ru>
2020-05-01 16:44:10 +0300

    Добавлены классы CaptchaCode и GlobalSettings. Работают

---
+++ src/main/java/main/model/CaptchaCode.java
@@ -0,0 +1,29 @@
+package main.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "captcha_codes")
+@NoArgsConstructor
+@Data
+@ToString
+public class CaptchaCode {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "time", nullable = false)
+    private Date time;
+
+    @Column(name = "code", nullable = false)
+    private String code;
+
+    @Column(name = "secret_code", nullable = false)
+    private String secretCode;
+}
---
+++ src/main/java/main/model/GlobalSettings.java
@@ -0,0 +1,27 @@
+package main.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "global_settings")
+@NoArgsConstructor
+@Data
+@ToString
+public class GlobalSettings {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "code", nullable = false)
+    private String code;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "value", nullable = false)
+    private String value;
+}
