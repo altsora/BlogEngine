fccc042eb681d7ee2de2c81eb335a7260f0e6795
altsora <vergun_a.s@mail.ru>
2020-05-03 19:21:31 +0300

    Added dependency thymeleaf. index.html starts

--- src/main/java/main/controller/DefaultController.java
+++ src/main/java/main/controller/DefaultController.java
@@ -1,15 +1,13 @@
 package main.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
-
-@RestController
+@Controller
 public class DefaultController {
 
     @RequestMapping("/")
     public String index() {
-        return (new Date()).toString();
+        return "index";
     }
 }
