11af6a4422cc702274a2b7ad363deeb4cb4a4f62
altsora <vergun_a.s@mail.ru>
2020-05-09 20:04:27 +0300

    added /api/init -> class Blog

--- src/main/java/main/controller/ApiGeneralController.java
+++ src/main/java/main/controller/ApiGeneralController.java
@@ -1,5 +1,6 @@
 package main.controller;
 
+import main.model.Blog;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,14 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiGeneralController {
 
     @GetMapping(value = "/api/init")
-    public ResponseEntity init() {
-        return new ResponseEntity("Hello", HttpStatus.OK);
+    public Blog init() {
+        return new Blog();
     }
 
     @GetMapping(value = "/api/settings")
     public ResponseEntity getSettings() {
         return ResponseEntity.status(HttpStatus.OK).body(null);
     }
-
 
 }
---
+++ src/main/java/main/model/Blog.java
@@ -0,0 +1,15 @@
+package main.model;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class Blog {
+    private String title = "DevPub";
+    private String subtitle = "Рассказы разработчиков";
+    private String phone = "+7 903 666-44-55";
+    private String email = "mail@mail.ru";
+    private String copyright = "Дмитрий Сергеев";
+    private String copyrightFrom = "2005";
+}
